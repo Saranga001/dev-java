a935eb439b0bcc794e667053d658bff611319df8
Saranga001 <srk10659@gmail.com>
2026-03-18 01:58:31 +0530

    My first java program

--- hello.java
+++ hello.java
@@ -1,6 +1,8 @@
 class hello {
   public static void main(String[] args){
-    System.out.println("Hello Java!");
-    System.out.println("Sarangaraj will start learning Java.");
+    System.out.println("Java: Hello Saranga!");
+    System.out.println("Me: Yes Java, I am here to learn you.");
+    System.out.println("Java: What is your age?");
+    System.out.println(5*5);
   }
 }
